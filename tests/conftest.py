acd4d2a1dd9a9d9635addd7cc91f37dff9775dfd
davidesanna <davide.sanna@statworx.com>
2022-03-31 21:10:44 +0200

    add further tests

---
+++ tests/conftest.py
@@ -0,0 +1,107 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# from alembic import command
+
+from app.config import settings
+from app.database.database import Base, get_db
+from app.main import app
+from app.schemas.schemas import UserResponse, PostTable
+from oauth2 import create_access_token
+
+# during testing we can hard code the variables
+SQLALCHEMY_DATABASE_URL = "postgresql://%s:%s@%s/%s" % (
+    settings.database_username,
+    settings.database_password,
+    settings.database_hostname,
+    settings.database_name + "_test",
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)  # sqlalchemy commands
+    Base.metadata.create_all(bind=engine)  # sqlalchemy commands
+    # command.upgrade("head") alembic commands
+    # command.downgrade("base") alembic commands
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "topogigio@gmail.com", "password": "test123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = UserResponse(**res.json())
+    new_user["password"] = user_data.password
+
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token(data={"user_id": test_user["id"]})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [
+        {
+            "title": "First Title",
+            "content": "first content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "Second Title",
+            "content": "second content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "First Title",
+            "content": "third content",
+            "owner_id": test_user["id"],
+        },
+    ]
+
+    def create_post_model(post):
+        return PostTable(**post)
+
+    posts = list(map(create_post_model, posts_data))
+
+    session.add_all(posts)
+
+    session.commit()
+
+    queried_posts = session.query(PostTable).all()
+
+    return queried_posts
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,7 @@
-from app.schemas.schemas import UserResponse
-from tests.database import (
-    client,
-    session,
-)  # we need to import both fixtures since one is dependent from the other
+from jose import jwt
+import pytest
+from app.schemas.schemas import UserResponse, Token
+from app.config import settings
 
 
 def test_root(client):
@@ -27,10 +26,36 @@ def test_create_user(client):
     )  # watch out to the url passed into client.post, it has to be /<name>/ and not /<name> otherwise, it redirects and returns a 307 first
 
 
-def test_login_user(client):
+def test_login_user(client, test_user):
     res = client.post(
-        "/login", data={"username": "topogigio@gmail.com", "password": "test123"}
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
     )  # in this case we use /<name> since the route does not have a prefix, and data instead of json
 
-    print(res.json())
+    login_res = Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=settings.algorithm
+    )
+    id = payload.get("user_id")
+
     assert res.status_code == 200
+    assert test_user.id == id
+    assert login_res.token_type == "bearer"
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "test123", 403),
+        ("topogigio@gmail.com", "wrong password", 403),
+        ("wrongemail@gmail.com", "wrong password", 403),
+        (None, "wrong password", 422),  # Unprocessible Entity, None is = Empty
+        ("topogigio@gmail.com", None, 422),  # Unprocessible Entity, None is = Empty
+    ],
+)
+def test_incorrect_login(client, email, password, status_code):
+
+    res = client.post("/login", data={"username": email, password: status_code})
+
+    assert res.status_code == status_code
+    # assert res.json()["detail"] == "Invalid Credentials"
